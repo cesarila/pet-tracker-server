034dbb141fff165bdf31b51f5ddb2abfca244a6f
Luke <cesarila@rose-hulman.edu>
2025-11-25 15:25:27 -0600

    WIP Broken start trying to test

---
+++ api_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// Made with consulting the following sources:
+// https://stackoverflow.com/questions/24455147/how-do-i-send-a-json-string-in-a-post-request-in-go
+// https://github.com/gin-gonic/examples/blob/master/basic/main_test.go
+// TODO: Change package name from main, see: https://appliedgo.net/testmain/
+func TestPostRoute(t *testing.T) {
+	router := setupRouter()
+
+	w := httptest.NewRecorder()
+	var jsonString = []byte(`{"new_pet_name": "cat1"}`)
+	req, _ := http.NewRequest("POST", "/pets", bytes.NewBuffer(jsonString))
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "ok", w.Body.String())
+
+}
