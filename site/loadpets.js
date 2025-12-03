0bebf8d544c1eeb4bace9d4f61749ba44091e037
Luke <cesarila@rose-hulman.edu>
2025-12-03 13:58:16 -0600

    add attribution for references used

--- site/loadpets.js
+++ site/loadpets.js
@@ -1,9 +1,16 @@
+// Resources referenced in writing this file: 
+// https://reqbin.com/code/javascript/car0zbiq/javascript-xmlhttprequest-example
+// https://www.w3schools.com/js/js_ajax_http.asp
+// https://www.w3schools.com/jsref/met_table_insertrow.asp
+
 const xhttp = new XMLHttpRequest();
 
 xhttp.onload = () => {
     const pets = JSON.parse(xhttp.response)
 
     let rowIndex = 0
+    
+    //Display the pet data
     const displayAreaElement = document.querySelector("table#petDisplay")
     Object.keys(pets).forEach(name => {
         let row = displayAreaElement.insertRow(rowIndex);
